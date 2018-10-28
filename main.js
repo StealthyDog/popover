7f12e9069896c987b636ade3e466cffedb7b1a68
Young <304172871@qq.com>
2018-10-29 00:30:08 +0800

    first submit

---
+++ main.js
@@ -0,0 +1,17 @@
+let event=true
+$(btn).on('click',function(){
+   if(event){
+     $(float).show()
+   }else{
+     $(float).hide()
+   }
+   event = !event
+})
+
+$(box).on('click',function(e){
+   e.stopPropagation()
+})
+$(document).on('click',function(){
+   $(float).hide()
+   event = true
+}) 
